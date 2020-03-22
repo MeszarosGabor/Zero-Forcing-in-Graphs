80a251473a419896fca8ddd4241337e116f2e0f5
Gabor Meszaros <jegkocka88@gmail.com>
2020-03-22 15:10:40 -0700

    binary pendant bugfixes

--- binary_pendant.py
+++ binary_pendant.py
@@ -13,3 +13,14 @@ level_1_binary_pendant_neighbors =\
     shift_pendant_indexes(pendant_neighbors, pn_size + 1)
 level_1_binary_pendant_neighbors[0].add(pn_size)
 level_1_binary_pendant_neighbors[pn_size + 1].add(pn_size)
+
+def get_level_n_binary_pendant_neighbors(level):
+    if level == 1:
+        return level_1_binary_pendant_neighbors
+    smaller = get_level_n_binary_pendant_neighbors(level - 1)
+    smaller_size = len(smaller)
+    left = deepcopy(smaller)
+    left[smaller_size // 2].add(smaller_size)
+    right = shift_pendant_indexes(smaller, smaller_size + 1)
+    right[smaller_size // 2].add(smaller_size)
+    return left + [{smaller_size // 2, smaller_size + smaller_size // 2 + 1}] + right
